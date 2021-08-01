97a888183dfca3837086e5f7721ce6952d023f75
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-08-01 14:15:18 -0300

    curso 01 - capitulo 04 | aula 02 completo

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,10 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
-  declarations: [PhotoComponent],
-  exports: [PhotoComponent],
-  imports: [HttpClientModule],
+  declarations: [PhotoComponent, PhotoListComponent],
+  imports: [HttpClientModule, CommonModule],
 })
 export class PhotosModule {}
