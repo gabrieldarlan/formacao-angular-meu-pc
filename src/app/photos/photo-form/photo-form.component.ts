bee8472ce7fad4e19e6e762486fdb11b871309c0
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-08-06 14:33:51 -0300

    curso 01 | capitulo 05 completo

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-photo-form',
+  // tslint:disable-next-line: component-selector
+  selector: 'ap-photo-form',
   templateUrl: './photo-form.component.html',
-  styleUrls: ['./photo-form.component.css']
+  styleUrls: ['./photo-form.component.css'],
 })
 export class PhotoFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
