9bd89a21421b081b3dcaacde7b890f2b1b2466ef
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-08-13 18:45:50 -0300

    curso 01 | capitulo 07 - aula 02 completa

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,22 +1,9 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { PhotoComponent } from './photo/photo.component';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoModule } from './photo/photo.module';
 
 @NgModule({
-  declarations: [
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    PhotosComponent,
-    FilterByDescription,
-    LoadButtonComponent,
-  ],
-  imports: [HttpClientModule, CommonModule],
+  imports: [PhotoModule, PhotoListModule, PhotoFormModule],
 })
 export class PhotosModule {}
